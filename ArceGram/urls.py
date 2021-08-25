500d2fe01a548b69c63c16ea319a24a27f867fcc
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2021-08-24 21:16:17 -0400

    Hello world in Django

--- ArceGram/urls.py
+++ ArceGram/urls.py
@@ -1,3 +1,5 @@
+"""ArceGram URLs module."""
+
 """ArceGram URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -15,7 +17,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+
+def hello_world(request):
+    """Return a greeting."""
+    return HttpResponse("Hello world!")
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('hello-world/', hello_world),
 ]
