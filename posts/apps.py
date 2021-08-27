d2fff7969b7f8aaea48bfd5939f1659aec2991c4
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2021-08-26 23:12:22 -0400

    App posts created, using templates, and using render

---
+++ posts/apps.py
@@ -0,0 +1,8 @@
+"""Posts aplication module"""
+from django.apps import AppConfig
+
+
+class PostsConfig(AppConfig):
+    """Post aplication settings"""
+    name = 'posts'  # name of the aplication
+    verbose_name="Posts" #name of the aplication in plural
