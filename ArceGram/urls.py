a59f1a46dabc02d71536761e824839bc38744404
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2021-08-29 13:13:16 -0500

    Models deleted

--- ArceGram/urls.py
+++ ArceGram/urls.py
@@ -15,13 +15,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+#Django
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
+
 from ArceGram import views as local_views
 from posts import views as posts_views
 
 
 urlpatterns = [
+    path('admin/',admin.site.urls),
     path('hello-world/', local_views.hello_world),
     path('sorted/',local_views.sort_integers),
     path('hi/<str:name>/<int:age>/',local_views.say_hi),
