60c421092368dbb4991a614b6edc649e681a531d
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2021-08-29 11:22:07 -0500

    Database Configurated

--- ArceGram/settings.py
+++ ArceGram/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'ArceGram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'arceGram',
+        'USER': 'root',
+        'PASSWORD': 'toor',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,16 @@
+"""posts models"""
+#Django
 from django.db import models
+from django.db.models.fields import CharField
 
-# Create your models here.
+#in this part classes in our database are gonna be the tables
+class User(models.Model):
+    """User model"""
+    email=models.EmailField(unique=True)
+    password=models.CharField(max_length=100)
+    first_name=models.CharField(max_length=100)
+    last_name=models.CharField(max_length=100)
+    bio=models.TextField(blank=True)
+    birthdate=models.DateField(blank=True,null=True)
+    created= models.DateTimeField(auto_now_add=True)
+    modified=models.DateTimeField(auto_now=True)
