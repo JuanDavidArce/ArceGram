d45d5b01b26747a2d481f4d108fb2a38f05d5930
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2021-08-31 17:53:08 -0500

    templates navbar and base creted

--- ArceGram/settings.py
+++ ArceGram/settings.py
@@ -61,7 +61,9 @@ ROOT_URLCONF = 'ArceGram.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR,'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -129,6 +131,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS= (os.path.join(BASE_DIR,'static'),)
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
