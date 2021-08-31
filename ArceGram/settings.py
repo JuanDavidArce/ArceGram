5b3a9491c5a228fa28d1cf12a217a5db1fca690c
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2021-08-31 18:44:12 -0500

    Login ready

--- ArceGram/settings.py
+++ ArceGram/settings.py
@@ -17,6 +17,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -143,3 +144,4 @@ STATICFILES_FINDERS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT= os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
+LOGIN_URL='/users/login/'
