6bbe72d84d43ecdab3bcdb63bf2364bcc4313b8b
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2021-08-30 21:21:15 -0500

    Post model created and added to admin

--- users/admin.py
+++ users/admin.py
@@ -11,9 +11,9 @@ from users.models import Profile
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     """Profile admin"""
-    list_display= ('pk','user','phone_number','website')
+    list_display= ('pk','user','phone_number','website','picture')
     list_display_links=('pk','user')
-    list_editable= ('phone_number', 'website')
+    list_editable= ('phone_number', 'website','picture')
     search_fields= ('user__email','user__first_name', 'user__last_name', 'phone_number','user__username')
     list_filter= ('created','modified','user__is_active','user__is_staff')
 
