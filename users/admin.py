ff3a6e57c4acf2518a82006a595f5806f941e33a
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2021-08-30 20:49:37 -0500

    UserAdmin, options and diplay configurated

--- users/admin.py
+++ users/admin.py
@@ -2,8 +2,8 @@
 
 #Django
 from django.contrib import admin
-
-
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 #Models
 from users.models import Profile
 
@@ -16,3 +16,32 @@ class ProfileAdmin(admin.ModelAdmin):
     list_editable= ('phone_number', 'website')
     search_fields= ('user__email','user__first_name', 'user__last_name', 'phone_number','user__username')
     list_filter= ('created','modified','user__is_active','user__is_staff')
+
+    fieldsets=(
+        ('Profile', {'fields':(('user','picture'),)}),
+        ('Extra Info',{'fields':(('website','phone_number'),('biography'))}),
+        ('Metadata',{'fields':(('created','modified'),)})
+        
+    )
+    readonly_fields=('created','modified')
+
+class ProfileInline(admin.StackedInline):
+    """Pofile in-line admin for users"""
+    model=Profile
+    can_delete=False
+    verbose_name_plural='Profiles'
+
+class UserAdmin(BaseUserAdmin):
+    """add profile admin to base user admin."""
+    inlines= (ProfileInline,)
+    list_display=(
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'is_active',
+        'is_staff',
+    )
+
+admin.site.unregister(User)
+admin.site.register(User,UserAdmin)
