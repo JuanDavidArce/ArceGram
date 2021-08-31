7ec0f7870d375e8d27545519d3931bdb037a414d
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2021-08-30 21:01:09 -0500

    New model post created an adde to db

--- posts/models.py
+++ posts/models.py
@@ -2,3 +2,23 @@
 
 #IMPORTANT: BY DEFAULT DJANGO ADD AN ID IN OUR TABLES
 #in this part classes in our database are gonna be the tables
+
+#Django
+from users.models import Profile
+from django.contrib.auth.models import User
+from django.db import models
+
+class Post(models.Model):
+    """Post model"""
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
+
+    title=models.CharField(max_length= 255)
+    photo=models.ImageField(upload_to='posts/photos')
+
+    created =models.DateTimeField(auto_now_add=True)
+    modified =models.DateTimeField(auto_now=True)
+
+    def __str__(self) :
+        """Return title and username"""
+        return '{} by @{}'.format(self.title,self.user.username)
