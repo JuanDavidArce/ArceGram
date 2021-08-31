1201f70dbeb4528ca91f32442032d5b5381ce75e
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2021-08-30 20:17:52 -0500

    Profile admin added

--- users/apps.py
+++ users/apps.py
@@ -1,6 +1,8 @@
+"""User app configuration"""
 from django.apps import AppConfig
 
 
 class UsersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
+    """user app config"""
     name = 'users'
+    verbose_name= "Users"
