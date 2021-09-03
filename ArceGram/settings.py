cd905eaf01f55843c42809e0e060b36758351681
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2021-09-03 15:23:10 -0500

    Middleware to check profile complete added

--- ArceGram/settings.py
+++ ArceGram/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'ArceGram.middleware.ProfileCompletionMiddleware',
 ]
 
 ROOT_URLCONF = 'ArceGram.urls'
