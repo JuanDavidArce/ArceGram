56a6bed73c81e994b3a486bdf19c0b7ddd5a99e2
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2021-09-03 21:05:25 -0500

    Update profile ready

--- users/views.py
+++ users/views.py
@@ -12,12 +12,31 @@ from django.contrib.auth.models import User
 #Exeption
 from django.db.utils import IntegrityError
 
+#Forms
+from users.forms import ProfileForm
+
 # Create your views here.
 
 def update_profile(request):
     """Update a user's profile view"""
-    profile= request.user.profile
-    return render(request,'users/update_profile.html',context={'profile':profile,'user':request.user})
+    profile=request.user.profile
+
+    if request.method=='POST':
+        form=ProfileForm(request.POST,request.FILES)
+        if form.is_valid():
+            data= form.cleaned_data
+            profile.website=data['website']
+            profile.phone_number=data['phone_number']
+            profile.biography=data['biography']
+            profile.picture=data['picture']
+            profile.save()
+
+            return redirect('update_profile')
+    else:
+        form=ProfileForm()
+
+    return render(request,'users/update_profile.html',
+    context={'profile':profile,'user':request.user,'form':form})
 
 def login_view(request):
     """Login view"""
