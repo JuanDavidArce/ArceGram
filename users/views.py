35df57757756d4eab92a289462d7e1e356ed2299
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2021-09-06 18:17:53 -0500

    Class based in views implemented

--- users/views.py
+++ users/views.py
@@ -28,7 +28,7 @@ def update_profile(request):
             profile.picture=data['picture']
             profile.save()
 
-            return redirect('update_profile')
+            return redirect('users:update_profile')
     else:
         form=ProfileForm()
 
@@ -44,7 +44,7 @@ def login_view(request):
         user=authenticate(request,username=username,password=password)
         if user:
             login(request,user)
-            return redirect('feed')
+            return redirect('posts:feed')
         else:
             return render(request,'users/login.html',{'error': 'Invalid username and password'})
        
@@ -54,14 +54,14 @@ def login_view(request):
 def logout_view(request):
     """Logout a user"""
     logout(request)
-    return redirect('login')
+    return redirect('users:login')
 
 def signup_view(request):
     if request.method== 'POST':
         form= SignupForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect ('login')
+            return redirect ('users:login')
     else:
         form=SignupForm()
 
