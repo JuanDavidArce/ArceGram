69de3fbe66907249f0b83566a3c317495594e23b
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2021-09-07 16:41:48 -0500

    Modification to paths

--- ArceGram/urls.py
+++ ArceGram/urls.py
@@ -28,6 +28,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/',admin.site.urls),
     path('',include(('posts.urls','posts'),namespace='posts')),
-    path('users',include(('users.urls','users'),namespace='users')),
+    path('users/',include(('users.urls','users'),namespace='users')),
 
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- users/urls.py
+++ users/urls.py
@@ -10,28 +10,28 @@ urlpatterns=[
 
     #Management
     path(
-        route='/login/',
+        route='login/',
         view=views.LoginView.as_view(),
         name='login'),
 
     path(
-        route='/logout/',
+        route='logout/',
         view=views.LogoutView.as_view(),
         name='logout'),
 
     path(
-        route='/signup/',
+        route='signup/',
         view=views.SignupView.as_view(),
         name='signup'),
 
     path(
-        route='/me/profile/',
+        route='me/profile/',
         view=views.UpdateProfileView.as_view(),
         name='update_profile'),
 
     #Posts
     path(
-        route='/profile/<str:username>/',
+        route='profile/<str:username>/',
         view=views.UserDetailView.as_view(),
         name='detail'
     )
