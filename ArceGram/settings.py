4b9c383583be69197783761954ff5dae5f446e45
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2021-09-06 21:06:45 -0500

    Login and Logout class ready

--- ArceGram/settings.py
+++ ArceGram/settings.py
@@ -146,3 +146,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT= os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
 LOGIN_URL='/users/login/'
+LOGIN_REDIRECT_URL='/'
+LOGOUT_REDIRECT_URL=LOGIN_URL
--- users/views.py
+++ users/views.py
@@ -9,6 +9,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls.base import reverse_lazy,reverse
 from django.views.generic import DetailView,FormView,UpdateView
 from django.urls import reverse_lazy
+from django.contrib.auth import views as auth_views
 
 #Models
 from django.contrib.auth.models import User
@@ -60,24 +61,12 @@ class UpdateProfileView(LoginRequiredMixin,UpdateView):
         
 
 
-def login_view(request):
+class LoginView(auth_views.LoginView):
     """Login view"""
-    if request.method=='POST':
-        username= request.POST['username']
-        password=request.POST['password']
-        
-        user=authenticate(request,username=username,password=password)
-        if user:
-            login(request,user)
-            return redirect('posts:feed')
-        else:
-            return render(request,'users/login.html',{'error': 'Invalid username and password'})
-       
-    return render(request,'users/login.html')
+    template_name='users/login.html'
 
-@login_required
-def logout_view(request):
-    """Logout a user"""
-    logout(request)
-    return redirect('users:login')
 
+
+class LogoutView(LoginRequiredMixin,auth_views.LogoutView):
+    """Logout View"""
+    template_name='users/logout.html'
