b8437a6b78204a2c974bb09d7158d74a0f144b28
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2021-09-07 15:40:05 -0500

    PostAdmin Modified

--- posts/admin.py
+++ posts/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 
 from posts.models import Post
 # Register your models here.
-admin.site.register(Post)
+
+# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """Post admin"""
+    list_display= ('pk','title','photo','created','modified')
+    list_display_links=('pk','created','modified')
+    list_editable= ('title', 'photo')
+
