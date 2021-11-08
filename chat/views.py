4861f9e3d65a409470fc7275c4ba608956be60db
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2021-11-07 20:30:51 -0500

    Some templates modifided and message button added

--- chat/views.py
+++ chat/views.py
@@ -3,6 +3,9 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 from chat.models import Thread
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render,redirect
+from django.views import View
 
 
 @login_required
@@ -12,3 +15,14 @@ def messages_page(request):
         'Threads': threads
     }
     return render(request, 'chat/messages.html', context)
+
+
+class New_thread(LoginRequiredMixin,View):
+    def post(self, request, *args, **kwargs):
+            conversation = Thread(first_person_id=request.POST['first_person_id'],second_person_id=request.POST['second_person_id'])  
+            conversations1= Thread.objects.all().filter(first_person_id=request.POST['first_person_id']).filter(second_person_id=request.POST['second_person_id'])
+            conversations2= Thread.objects.all().filter(first_person_id=request.POST['second_person_id']).filter(second_person_id=request.POST['first_person_id'])
+            if len(conversations1)==0 and len(conversations2)==0: 
+                conversation.save()
+
+            return redirect("chat:chat")
