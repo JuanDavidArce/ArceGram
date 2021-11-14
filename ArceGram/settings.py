01d7f1c08b45fcb1fd4f5e9c89beed63ba36ae5c
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2021-11-14 12:41:32 -0500

    Procfile edited to asgi

--- ArceGram/settings.py
+++ ArceGram/settings.py
@@ -167,6 +167,7 @@ STATICFILES_FINDERS = [
 STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 # dtpjikaq7
