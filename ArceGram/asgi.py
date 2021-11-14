f3265e71d7a0be244621bbf3e1696341c1dfa3c3
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2021-11-14 10:27:00 -0500

    setting to heroku

--- ArceGram/asgi.py
+++ ArceGram/asgi.py
@@ -8,16 +8,19 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
+import django
 import chat.routing
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ArceGram.settings')
+django.setup()
 
 
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
+    'https': get_asgi_application(),
+    # 'http': get_asgi_application(),
     'websocket': AuthMiddlewareStack(
         URLRouter(
             chat.routing.websocket_urlpatterns
--- ArceGram/wsgi.py
+++ ArceGram/wsgi.py
@@ -6,7 +6,6 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
-
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -14,5 +13,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ArceGram.settings')
 
 application = get_wsgi_application()
-# from whitenoise.django import DjangoWhiteNoise  
-# application = DjangoWhiteNoise(application)
