fa341ffc07b60f0a2554021d3665bb56e4687380
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2021-11-14 13:21:40 -0500

    Procfile edited to asgi

--- ArceGram/asgi.py
+++ ArceGram/asgi.py
@@ -11,6 +11,7 @@ import os
 import django
 import chat.routing
 from django.core.asgi import get_asgi_application
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
