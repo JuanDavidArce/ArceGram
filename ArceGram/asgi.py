a3613cd6fc144e7cbc4ce8f489a735323f874ab6
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2021-11-14 13:23:52 -0500

    Procfile edited to asgi

--- ArceGram/asgi.py
+++ ArceGram/asgi.py
@@ -8,6 +8,8 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ArceGram.settings')
 import django
 import chat.routing
 from django.core.asgi import get_asgi_application
@@ -15,8 +17,6 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ArceGram.settings')
 
 
 application = ProtocolTypeRouter({
